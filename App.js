5180e0e8c4e2d9e69a5a68e3cced7e1e5dda0a84
duncan359 <duncan02359@gmail.com>
2018-07-25 15:35:52 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+  StackNavigator,
+} from 'react-navigation';
+import SearchPage from './SearchPage';
+import SearchResults from './SearchResults';
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+ 
+const App = StackNavigator({
+  Home: { screen: SearchPage },
+  Results: { screen: SearchResults },
+});
+export default App;
+
+const styles = StyleSheet.create({
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    marginTop: 65,
+  },
+});
